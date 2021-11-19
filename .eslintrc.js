74c013c62afddd2b54a1f582a0c2e7ed6e12f9a1
Christie Maduna <ferggiee@gmail.com>
2021-11-19 15:28:23 +0200

    start

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-essential", "@vue/standard"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "space-before-function-paren": 0,
+    quotes: "off",
+  },
+};
